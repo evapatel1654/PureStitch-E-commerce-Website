10704610e361fcef7e5251840d7bdaf5cbb49114
Eva Patel <133888581+evapatel1654@users.noreply.github.com>
2025-05-14 10:18:20 +0530

    Add files via upload

---
+++ wp-content/plugins/blockart-blocks/dist/slider.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5b8c509ef5cdead58219');
---
+++ wp-content/plugins/blockart-blocks/dist/timeline.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd5f4871b38993abb953a');
