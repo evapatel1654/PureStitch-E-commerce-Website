75a99318bfbc5731a16089c5c6424aed8db4883f
Eva Patel <133888581+evapatel1654@users.noreply.github.com>
2025-05-14 10:16:02 +0530

    Add files via upload

---
+++ wp-content/plugins/blockart-blocks/dist/blocks-17.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-preferences', 'wp-primitives', 'wp-url'), 'version' => '0281a40aa46c3a858094');
---
+++ wp-content/plugins/blockart-blocks/dist/dashboard.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'e13411ba2aad83b94896');
